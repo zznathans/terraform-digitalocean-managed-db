e8fefbe9086b5834bf8d90d941cc42d39d2da5e3
nathan <nathan@zswap.net>
2026-03-21 17:21:05 -0400

    fix(ci): fmt

--- cluster.tf
+++ cluster.tf
@@ -1,17 +1,16 @@
 resource "digitalocean_database_cluster" "cluster" {
-    name       = "${var.stack_name}-db"
-    engine     = var.engine
-    version    = var.engine_version
-    region     = var.region
-    size      = var.instance_size
-    node_count = var.node_count
-    project_id = var.project_id
+  name       = "${var.stack_name}-db"
+  engine     = var.engine
+  version    = var.engine_version
+  region     = var.region
+  size       = var.instance_size
+  node_count = var.node_count
+  project_id = var.project_id
 
-    dynamic "backup_restore" {
-        for_each = var.backup_source != "" ? [1] : []
-        content {
-            database_name = var.backup_source
-        }
+  dynamic "backup_restore" {
+    for_each = var.backup_source != "" ? [1] : []
+    content {
+      database_name = var.backup_source
     }
+  }
 }
-
--- vars.tf
+++ vars.tf
@@ -1,76 +1,76 @@
 variable "do_token" {
-    type = string
-    default = ""
+  type    = string
+  default = ""
 }
 
 variable "stack_name" {
-    type = string
+  type = string
 }
 
 variable "db_users" {
-    type = list
+  type = list(any)
 }
 
 variable "db_names" {
-    type = list
+  type = list(any)
 }
 
 variable "conn_pool_size" {
-    type = int
-    default = 5
+  type    = int
+  default = 5
 }
 
 variable "conn_pool_mode" {
-    type = string
-    default = "transaction"
+  type    = string
+  default = "transaction"
 }
 
 variable "engine_version" {
-    type = string
+  type = string
 }
 
 variable "engine" {
-    type = string
+  type = string
 }
 
 variable "instance_size" {
-    type = string
-    default = "db-s-1vcpu-1gb"
+  type    = string
+  default = "db-s-1vcpu-1gb"
 }
 
 variable "region" {
-    type = string
+  type = string
 }
 
 variable "node_count" {
-    type = int
-    default = 1
+  type    = int
+  default = 1
 }
 
 variable "backup_source" {
-    type = string
+  type = string
 }
 
 variable "project_id" {
-    type = string
+  type = string
 }
 
 variable "firewall_droplets" {
-    type = list
-    default = []
+  type    = list(any)
+  default = []
 }
 
 variable "firewall_tags" {
-    type = list
-    default = []
+  type    = list(any)
+  default = []
 }
 
 variable "firewall_k8s" {
-    type = list
-    default = []
+  type    = list(any)
+  default = []
 }
 
 variable "firewall_ips" {
-    type = list
-    default = []
+  type    = list(any)
+  default = []
 }
