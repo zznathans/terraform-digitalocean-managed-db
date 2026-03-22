edc2a35d976de3b21d1aae6b2b112ffef399cafc
nathan <nathan@zswap.net>
2026-03-22 14:46:02 -0400

    use pool info

--- users.tf
+++ users.tf
@@ -1,3 +1,11 @@
+locals {
+  use_pools = length(var.db_names) > 0
+
+  gcp_secret_iter = local.use_pools ? local.pool_combinations : {
+    for u in var.db_users : u => { user = u, db_name = "" }
+  }
+}
+
 resource "digitalocean_database_user" "user" {
   for_each   = toset(var.db_users)
   cluster_id = digitalocean_database_cluster.cluster.id
@@ -5,10 +13,10 @@ resource "digitalocean_database_user" "user" {
 }
 
 resource "google_secret_manager_regional_secret" "psql-user" {
-  for_each  = var.push_gcp_secret ? toset(var.db_users) : toset([])
+  for_each  = var.push_gcp_secret ? local.gcp_secret_iter : {}
   project   = var.gcp_project
   location  = var.gcp_region
-  secret_id = "${var.stack_name}-db-user-${each.value}"
+  secret_id = local.use_pools ? "${var.stack_name}-db-${each.key}" : "${var.stack_name}-db-user-${each.key}"
 
   labels = {
     managed-by = "terraform"
@@ -17,12 +25,12 @@ resource "google_secret_manager_regional_secret" "psql-user" {
 }
 
 resource "google_secret_manager_regional_secret_version" "psql-user" {
-  for_each    = var.push_gcp_secret ? toset(var.db_users) : toset([])
+  for_each    = var.push_gcp_secret ? local.gcp_secret_iter : {}
   secret      = google_secret_manager_regional_secret.psql-user[each.key].id
   secret_data = jsonencode({
-    db_host     = digitalocean_database_cluster.cluster.host
-    db_port     = digitalocean_database_cluster.cluster.port
-    db_user     = digitalocean_database_user.user[each.key].name
-    db_password = digitalocean_database_user.user[each.key].password
+    db_host     = local.use_pools ? digitalocean_database_connection_pool.pool[each.key].host : digitalocean_database_cluster.cluster.host
+    db_port     = local.use_pools ? digitalocean_database_connection_pool.pool[each.key].port : digitalocean_database_cluster.cluster.port
+    db_user     = digitalocean_database_user.user[each.value.user].name
+    db_password = digitalocean_database_user.user[each.value.user].password
   })
 }
