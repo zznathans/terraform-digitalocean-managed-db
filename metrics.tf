f6c97506dbefa2c53162e6f073b061e52c40e0fe
nathan <nathan@zswap.net>
2026-03-22 17:00:30 -0400

    feat(metrics): add Google Secret Manager resources for database metrics

---
+++ metrics.tf
@@ -0,0 +1,22 @@
+data "digitalocean_database_metrics_credentials" "metrics" {}
+
+resource "google_secret_manager_regional_secret" "metrics" {
+  project   = var.gcp_project
+  location  = var.gcp_region
+  secret_id = "${var.stack_name}-db-metrics"
+
+  labels = {
+    managed-by = "terraform"
+    app        = var.stack_name
+  }
+}
+
+resource "google_secret_manager_regional_secret_version" "metrics" {
+  secret      = google_secret_manager_regional_secret.metrics.id
+  secret_data = jsonencode({
+    metrics_username = data.digitalocean_database_metrics_credentials.metrics.username
+    metrics_password = data.digitalocean_database_metrics_credentials.metrics.password
+    metrics_endpoint = digitalocean_database_cluster.cluster.metrics_endpoints[0].host
+    metrics_port    = digitalocean_database_cluster.cluster.metrics_endpoints[0].port
+  })
+}
